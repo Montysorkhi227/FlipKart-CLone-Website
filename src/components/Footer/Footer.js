dc162f9f3ab563193c7bbbc323c4f923fa080e1c
Vishal Rana <montysorkhi227@gmail.com>
2026-05-08 00:29:07 +0530

    second commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,46 +9,46 @@ const Footer = () => {
         <div className="footer__section">
           <h4 className="footer__heading">ABOUT</h4>
           <ul className="footer__list">
-            <li><a href="#">Contact Us</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Careers</a></li>
-            <li><a href="#">Flipkart Stories</a></li>
-            <li><a href="#">Press</a></li>
-            <li><a href="#">Flipkart Wholesale</a></li>
-            <li><a href="#">Corporate Information</a></li>
+            <li><a href="/">Contact Us</a></li>
+            <li><a href="/">About Us</a></li>
+            <li><a href="/">Careers</a></li>
+            <li><a href="/">Flipkart Stories</a></li>
+            <li><a href="/">Press</a></li>
+            <li><a href="/">Flipkart Wholesale</a></li>
+            <li><a href="/">Corporate Information</a></li>
           </ul>
         </div>
 
         <div className="footer__section">
           <h4 className="footer__heading">HELP</h4>
           <ul className="footer__list">
-            <li><a href="#">Payments</a></li>
-            <li><a href="#">Shipping</a></li>
-            <li><a href="#">Cancellation & Returns</a></li>
-            <li><a href="#">FAQ</a></li>
-            <li><a href="#">Report Infringement</a></li>
+            <li><a href="/">Payments</a></li>
+            <li><a href="/">Shipping</a></li>
+            <li><a href="/">Cancellation & Returns</a></li>
+            <li><a href="/">FAQ</a></li>
+            <li><a href="/">Report Infringement</a></li>
           </ul>
         </div>
 
         <div className="footer__section">
           <h4 className="footer__heading">CONSUMER POLICY</h4>
           <ul className="footer__list">
-            <li><a href="#">Cancellation & Returns</a></li>
-            <li><a href="#">Terms Of Use</a></li>
-            <li><a href="#">Security</a></li>
-            <li><a href="#">Privacy</a></li>
-            <li><a href="#">Sitemap</a></li>
-            <li><a href="#">Grievance Redressal</a></li>
-            <li><a href="#">EPR Compliance</a></li>
+            <li><a href="/">Cancellation & Returns</a></li>
+            <li><a href="/">Terms Of Use</a></li>
+            <li><a href="/">Security</a></li>
+            <li><a href="/">Privacy</a></li>
+            <li><a href="/">Sitemap</a></li>
+            <li><a href="/">Grievance Redressal</a></li>
+            <li><a href="/">EPR Compliance</a></li>
           </ul>
         </div>
 
         <div className="footer__section">
           <h4 className="footer__heading">SOCIAL</h4>
           <ul className="footer__list">
-            <li><a href="#">Facebook</a></li>
-            <li><a href="#">Twitter</a></li>
-            <li><a href="#">YouTube</a></li>
+            <li><a href="/">Facebook</a></li>
+            <li><a href="/">Twitter</a></li>
+            <li><a href="/">YouTube</a></li>
           </ul>
         </div>
 
@@ -64,8 +64,10 @@ const Footer = () => {
               Karnataka, India
             </p>
           </div>
+
           <div className="footer__registered">
             <h4 className="footer__heading">REGISTERED OFFICE ADDRESS</h4>
+
             <p className="footer__address">
               Flipkart Internet Private Limited,<br />
               Sy No. 90/1, 2nd Floor,<br />
@@ -75,8 +77,13 @@ const Footer = () => {
               Varthur Hobli, Bengaluru East,<br />
               Bengaluru Urban District,<br />
               Karnataka Pin Code - 560103<br />
-              <a href="#">CIN : U51109KA2012PTC066107</a><br />
-              <a href="#">Telephone: 044-45614700</a>
+
+              <a href="/">CIN : U51109KA2012PTC066107</a>
+              <br />
+
+              <a href="tel:04445614700">
+                Telephone: 044-45614700
+              </a>
             </p>
           </div>
         </div>
@@ -88,29 +95,34 @@ const Footer = () => {
       {/* Bottom */}
       <div className="footer__bottom">
         <div className="footer__bottom-links">
-          <a href="#">
+          <a href="/">
             <span className="footer__icon">🛒</span>
             Become a Seller
           </a>
-          <a href="#">
+
+          <a href="/">
             <span className="footer__icon">⭐</span>
             Advertise
           </a>
-          <a href="#">
+
+          <a href="/">
             <span className="footer__icon">🎁</span>
             Gift Cards
           </a>
-          <a href="#">
+
+          <a href="/">
             <span className="footer__icon">❓</span>
             Help Center
           </a>
         </div>
+
         <p className="footer__copy">© 2007-2024 Flipkart.com</p>
       </div>
 
       {/* Payment Methods */}
       <div className="footer__payments">
         <span className="footer__pay-label">Payment Methods</span>
+
         <div className="footer__pay-icons">
           <span className="pay-icon">Visa</span>
           <span className="pay-icon">Mastercard</span>
